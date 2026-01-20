0c68ad8b07f92254b966a593c91a7fe1f6a7e569
Ranjana-Khatiwada <ranjanakhatiwada62@gmail.com>
2026-01-20 07:20:35 +0545

    First JavaScript File added

---
+++ file.js
@@ -0,0 +1,35 @@
+alert('Script loaded');
+alert('Ranjana Khatiwada'); /* Alert Message */
+
+var variableName = 5; /* variableValue */
+alert('5');
+
+var variableName1 = "Ranjana";
+var variableName2 = "Khatiwada";
+var variableName3 = variableName1 + " " + variableName2;
+alert(variableName3);    /* Display alert message with both firstname and lastname */
+
+function scriptLoaded() {
+    alert('Script loaded');
+}
+
+function addition() {
+    var num1 = 5;
+    var num2 = 6;
+
+    var num3 = num1 + num2; 
+    alert(num3);
+}
+
+addition ();
+addition ();
+
+/* Function 1 */
+var firstname = "Ranjana";
+var lastname = "Khatiwada";
+
+var fullname = firstname + " " + lastname;
+alert(fullname);
+
+/* Function 2 */
+alert("Hello World!");
